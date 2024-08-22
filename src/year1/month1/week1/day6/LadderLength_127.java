267dadfec957ed60a8839c2ac06ec7cc8ceabf1c
cwh <bygzyx@outlook.com>
2024-08-22 22:14:37 +0800

    2024/08/22

--- src/year1/month1/week1/day6/LadderLength_127.java
+++ src/year1/month1/week1/day6/LadderLength_127.java
@@ -2,45 +2,42 @@ package year1.month1.week1.day6;
 
 import java.util.*;
 
-public class String_Concatenation_C {
+public class LadderLength_127 {
     public static void main(String[] args) {
-
+        Solution_127 solution127 = new Solution_127();
+        int i=solution127.ladderLength("hit","cog",new ArrayList<String>(Arrays.asList(new String[]{"hot", "dot", "dog", "lot", "log", "cog"})));
     }
 }
-class Solution_scc{
-    //广度优先遍历所有字串
+class Solution_127{
+    //广度优先遍历所有字串(要记级别)
     //将符合要求的(只有一个不同)加入queue和path
     //然后将包含终点的路径加入结果
     //遍历结果找出长度最短
-    List<List<String>> result = new ArrayList<>();
     List<String> path = new ArrayList<>();
-    public int stringConcate(String beginStr, String endStr, List<String> wordList){
-        Set<String> set = new HashSet<>(wordList);
-        set.add(endStr);
+    public int ladderLength(String beginStr, String endStr, List<String> wordList){
+        int len = wordList.size();
+        boolean[] visited = new boolean[len];
 
-        path.add(beginStr);
         Queue<String> queue = new ArrayDeque<>();
         queue.add(beginStr);
 
-        while (!queue.isEmpty() && !set.isEmpty()){
+        while (!queue.isEmpty()){
             String cur = queue.poll();
-            for (String str : set){
+            path.add(cur);
+
+            for (int i=0; i<len; i++){
+                if (visited[i])continue;
+                String str = wordList.get(i);
                 if (compareM(cur, str)){
-                    path.add(str);
-                    set.remove(str);//移出set
+                    queue.add(str);
+                    visited[i]=true;
                     if (str.equals(endStr)){
-                        result.add(path);
+                        return path.size()+1;
                     }
-                    path.remove(str);//回溯
                 }
             }
         }
-
-        int min=Integer.MAX_VALUE;
-        for (List<String> res : result){
-            min=Math.min(min, res.size());
-        }
-        return min==Integer.MAX_VALUE ? 0 : min;
+        return 0;
     }
 
     private boolean compareM(String str1, String str2) {
@@ -50,6 +47,4 @@ class Solution_scc{
         }
         return count==str1.length()-1;
     }
-
-
 }
