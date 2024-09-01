6f4c6eb377a99dc1ee86aceaefc84e35d0a6ebd1
cwh <bygzyx@outlook.com>
2024-09-01 23:32:29 +0800

    2024/09/01

--- src/year1/month1/week2/day1/RedBlackTree_C.java
+++ src/year1/month1/week2/day1/RedBlackTree_C.java
@@ -1,58 +1,149 @@
 package year1.month1.week2.day1;
 
+import com.sun.org.apache.bcel.internal.generic.ARETURN;
+
 public class RedBlackTree_C {
+    private static final boolean isMultiMap=false;
     enum Flag{
         RED,BLACK
     }
-    class Node {
+    class Node{
         int val;
         Flag flag;
         Node fa,left,right;
-        Node(int val){
+        public Node(int val){
             this.val=val;
-            this.flag=Flag.RED;
+            flag=Flag.RED;
         }
     }
     private Node root;
-    private Node find(Node root, int val){
+    Node find(Node root, int val){
         while (root!=null){
-            if (val< root.val)
+            if (val<root.val)
                 root=root.left;
-            else if (val> root.val)
+            else if (val>root.val)
                 root=root.right;
-            else
-                return root;
+            else return root;
         }
         return null;
     }
-    //左旋:新根有左放旧根右
-    //右旋:新根有右放旧根左
-    Node rightRotate(Node y){
-        //四步走,改变x和y的高度
-        Node x=y.left;
-        Node T2=x.right;
-        x.right=y;
-        y.left=T2;
-        return x;
-    }
-    Node leftRotate(Node y){
-        //四步走,改变x和y的高度
-        Node x=y.right;
-        Node T2=x.left;
-        x.left=y;
-        y.right=T2;
-        return x;
-    }
-    Node insert(Node node,int val){
-        //正常插入
-        if (node==null)return new Node(val);
 
-        if (val<node.val)
-            node.left=insert(node.left,val);
-        else if (val>node.val)
-            node.right=insert(node.right,val);
-        else return node;
+    //插入并且连接
+    void insertAndLink(Node root, Node node){
+        if (node.val<root.val){
+            if (root.left!=null)
+                insertAndLink(root.left,node);
+            else{
+                root.left=node;
+                node.fa=root;
+            }
 
-        return node;
+        } else {
+            if (root.right !=null)
+                insertAndLink(root.right,node);
+            else {
+                root.right=node;
+                node.fa=root;
+            }
+        }
+    }
+
+    //连接父子关系, dir为true则为左
+    void link(Node s, Node t, boolean dir){
+        if (s==null || s==t)return;
+
+        if (t!=null)t.fa=s;    //就算t为空也不能返回,先连上
+        if (dir)
+            s.left=t;
+        else
+            s.right=t;
+    }
+
+    void linkLeft(Node s, Node t){
+        link(s,t,true);
+    }
+    void linkRight(Node s,Node t){
+        link(s,t,false);
+    }
+    void linkReplace(Node s,Node t){
+        link(s.fa,t,s.fa!=null && s.fa.left==s);
+    }
+    void rotateLL(Node root){ //右旋:新根右放旧根左
+        Node c1=root.left;
+        linkReplace(root,c1); //c1顶掉root
+        linkLeft(root,c1.right);   //新根右放旧根左
+        linkRight(c1,root);       //旧根放右
+    }
+    void rotateRR(Node root){ //左旋:新根左放旧根右
+        Node c1=root.right;
+        linkReplace(root,c1); //c1顶掉root
+        linkRight(root,c1.left);   //新根左放旧根右
+        linkLeft(c1,root);       //旧根放左
+    }
+    void rotateLR(Node root){
+        Node c1=root.left;   //先左旋
+        Node c2=c1.right;
+        linkLeft(root,c2);
+        linkRight(c1,c2.left);
+        linkLeft(c2,c1);
+        rotateRR(root);
+    }
+    void rotateRL(Node root){
+        Node c1=root.right;   //先右旋
+        Node c2=c1.left;
+        linkRight(root,c2);
+        linkLeft(c1,c2.right);
+        linkRight(c2,c1);
+        rotateLL(root);
+    }
+    void rotate(Node pp, Node p, Node node){
+        if (pp.left==p){
+            if (p.left==node)
+                rotateLL(pp);
+            else
+                rotateLR(pp);
+        } else if (pp.right==p) {
+            if (p.right==node)
+                rotateRR(pp);
+            else
+                rotateRL(pp);
+        }
+    }
+    void adjust(Node node){
+        Node p=node.fa;
+        if (p==root)   //1.父根,黑
+            p.flag=Flag.BLACK;
+        if (p.flag==Flag.BLACK)return;
+
+        Node pp=p.fa;
+        Node unc= pp.left==p ? pp.right : pp.left;
+        if (unc!=null && unc.flag==Flag.RED){  //2.叔红, 直接变色
+            pp.flag=Flag.RED;
+            unc.flag=Flag.BLACK;
+            p.flag=Flag.BLACK;
+            adjust(pp); //将pp提交上去继续递归
+            return;
+        }
+
+        rotate(pp,p,node);  //3.叔无, 祖父红, 祖父的父黑(因为祖父的父已经不知道是谁了)
+        pp.flag=Flag.RED;
+        if (pp.fa!=null)pp.fa.flag=Flag.BLACK;
+    }
+    void insert(Node node){ //点
+        if (node==null)return;
+        if (root==null){
+            root=node;
+            node.flag=Flag.BLACK;
+            return;
+        }
+        if (root==node)return;
+        insertAndLink(root,node);
+        adjust(node);
+        while (root.fa != null)root=root.fa; //更新root, 可能被旋下来了, 一直更新
+    }
+    void insert(int val){  //值
+        Node node=find(root,val);
+        if (!isMultiMap && node!=null)return;
+        insert(new Node(val));
     }
 }
